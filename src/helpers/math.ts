ba80c00d2ac6b4c07cc5f054d0194c38c66f898a
ShoobyD <baruch@pic-time.com>
2024-12-14 22:45:48 +0200

    math polar utils

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -3,6 +3,11 @@ interface IPoint {
 	y: number;
 }
 
+interface IPolarPoint {
+	radius: number;
+	theta: number;
+}
+
 export function lerp( a: number, b: number, t: number ): number {
 	return a + ( b - a ) * t;
 }
@@ -15,6 +20,17 @@ export function distance( a: IPoint, b: IPoint ): number {
 	return Math.hypot( a.x - b.x, a.y - b.y );
 }
 
+export function norm( p: IPoint ): number {
+	return distance( p, { x: 0, y: 0 } );
+}
+
+export function scale( p: IPoint, factor: number ): IPoint {
+	return {
+		x: p.x * factor,
+		y: p.y * factor,
+	};
+}
+
 export function changeAbsoluteValue( value: number, diff: number ): number {
 	return value + Math.sign( value ) * diff;
 }
@@ -23,6 +39,13 @@ export function polarToCartesian( radius: number, theta: number ): IPoint {
 	return {
 		x: radius * Math.cos( theta ),
 		y: radius * Math.sin( theta ),
+	};
+}
+
+export function cartesianToPolar( p: IPoint ): IPolarPoint {
+	return {
+		radius: norm( p ),
+		theta : Math.atan2( p.y, p.x ),
 	};
 }
 
